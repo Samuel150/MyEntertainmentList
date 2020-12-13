1b532d013344fe379758127197ff8e3c872c821f
MateoAltamirano <mateo.altamirano.1998@gmail.com>
2020-12-13 19:28:15 -0400

    fix: conflicts

--- material-kit-react-master/src/index.js
+++ material-kit-react-master/src/index.js
@@ -21,23 +21,12 @@ var hist = createBrowserHistory();
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
-<<<<<<< HEAD
-      <Route path="/landing-page" component={LandingPage} />
-      <Route path="/profile-page" component={ProfilePage} />
-      <Route path="/login-page" component={LoginPage} />
-      <Route path="/mylists-page" component={MyLists} />
-      <Route path="/newslist-page" component={NewsList} />
-      <Route path="/signup-page" component={SignUpPage} />
-
-      <Route path="/" component={Components} />
-=======
       <Route path="/landing" component={LandingPage} />
       <Route path="/profile" component={ProfilePage} />
       <Route path="/login" component={LoginPage} />
       <Route path="/signUp" component={SignUpPage} />
       <Route exact path="/" component={Components} />
       <Route component={NotFoundPage} />
->>>>>>> f3c638d1a3d14a14fa118dae325126401013abf4
     </Switch>
   </Router>,
   document.getElementById("root")
